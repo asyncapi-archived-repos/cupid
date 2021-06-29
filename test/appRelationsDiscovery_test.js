c3b40b38d6844fcb368f3b1ab101e26f9583e35f
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-06-29 16:32:06 +0530

    feat: add test for mermaid and plantUML syntax

--- test/appRelationsDiscovery_test.js
+++ test/appRelationsDiscovery_test.js
@@ -7,10 +7,19 @@ chai.use(chaiAsPromised);
 const {getRelations} = require('../lib/appRelationsDiscovery');
 const {getAsyncApiExamples} = require('./testsUtil'); 
 
+const outputMermaid = 'graph TD\n server1[(mqtt://localhost:1883)]\nFlightMonitorService[Flight Monitor Service]\nFlightMonitorService -- flight/update --> server1\nFlightNotifierService[Flight Notifier Service]\nserver1 -- flight/update --> FlightNotifierService\nFlightSubscriberService[Flight Subscriber Service]\nFlightSubscriberService -- flight/queue --> server1\nserver1 -- flight/queue --> FlightMonitorService';
+const outputPlantUML = '@startuml\ntitle Classes - Class Diagram\n\nclass server1 { \n url: mqtt://localhost:1883 \n protocol: mqtt\n}\nFlightMonitorService --|> server1:flight/update\nserver1 --|> FlightNotifierService:flight/update\nFlightSubscriberService --|> server1:flight/queue\nserver1 --|> FlightMonitorService:flight/queue\n@enduml';
+
 describe('appRelationDiscovery', function() {
-  it('should return a map of relations', async function() {
-    const flightServiceDocs = await getAsyncApiExamples();
-    const defaultOutput = await getRelations(flightServiceDocs);
-    expect(typeof defaultOutput).to.be.equal('object');
+  it('should return the correct mermaid syntax for the given docs', async function() {
+    const flightServiceDocs = getAsyncApiExamples();
+    const output = await getRelations(flightServiceDocs,{syntax: 'mermaid'});
+    expect(output).to.be.equal(outputMermaid);
+  });
+
+  it('should return the correct plantUML syntax for the given docs', async function() {
+    const flightServiceDocs = getAsyncApiExamples();
+    const output = await getRelations(flightServiceDocs,{syntax: 'plantUML'});
+    expect(output).to.be.equal(outputPlantUML);
   });
 });
